0e49e16640479c859e3b36e52ffc121c01eb4db6
projection-part <61749356+projection-part@users.noreply.github.com>
2020-06-15 02:58:00 +0530

    is this okay

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<link rel="stylesheet" href="common_files/css/bootstrap.min.css">
 	<link rel="stylesheet" href="common_files/css/animate.css">
 	<link rel="stylesheet" href="common_files/css/fontawesome.min.css">
+	<link rel="stylesheet" href="css/index.css">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<script src="common_files/js/jquery.js"></script>
 	<script src="common_files/js/popper.js"></script>
@@ -16,6 +17,11 @@
 <body>
 <?php
 require_once("assist/nav.php");
+
+?>
+
+<?php
+require_once("assist/footer.php");
 
 ?>
 </body>
